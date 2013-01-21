658fd2484bf78eb26097ec6d2f341de7a6a174a9
Eric Xiao <exiao@buynear.me>
2013-01-21 14:43:41 -0800

    delete groups, add feedback

--- cc/ccapp/forms.py
+++ cc/ccapp/forms.py
@@ -9,4 +9,8 @@ class EmailForm(forms.Form):
     # this will not be the username as well for a facebook account
     email = forms.EmailField()
 
-
+class FeedbackForm(forms.Form):
+    email = forms.EmailField(label="Email")
+    message = forms.CharField(label="Feedback", required=True, widget=forms.Textarea(attrs={
+        'placeholder':'e.g. I had a hard time selling my item, because the buyer flaked on me :('
+    }))
--- cc/urls.py
+++ cc/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from ccapp.views import ContactView, AboutView, MainView, FriendsView
+from ccapp.views import ContactView, AboutView, MainView, FeedbackView, ThanksView
 from django.views.generic import TemplateView
 
 admin.autodiscover()
@@ -17,7 +17,8 @@ urlpatterns = patterns('',
     url(r'^about/$', AboutView.as_view()),
     url(r'^contact/$', ContactView.as_view()),
     url(r'^privacy/', TemplateView.as_view(template_name = 'privacy_policy.html')),
-    url(r'^feedback/', TemplateView.as_view(template_name = 'feedback.html')),
+    url(r'^feedback/', FeedbackView.as_view(), name="feedback"),
+    url(r'^thanks/', ThanksView.as_view(), name='thanks'),
     url(r'^terms/', TemplateView.as_view(template_name= 'terms.html')),
     url(r'^(?P<pid>\d+)$','ccapp.views.showpostIFS'),
 
@@ -26,6 +27,8 @@ urlpatterns = patterns('',
     url(r'^accounts/', include('django_facebook.auth_urls')),
     url(r'^fb_import/$', 'ccapp.views.fb_import', name='fb_import'),
     url(r'^account_setup/$', 'ccapp.views.account_setup', name='account_setup'),
+    url(r'^verify_user/(?P<auth_key>[\w\+%_& ]+)/$','ccapp.views.verify_user'),
+    url(r'^change_email/(?P<auth_key>[\w\+%_& ]+)/$','ccapp.views.change_email'),
 
     #ADMIN/DEBUG
     url(r'^friends/debug/$', 'ccapp.views.friendslist', name='test_friends'),
@@ -66,7 +69,6 @@ urlpatterns = patterns('',
     url(r'^canvas/', 'ccapp.views.canvas'),
     url(r'^user_posts/', 'ccapp.views.user_posts'),
     url(r'^bookmark/', 'ccapp.views.bookmark_post'),
-    url(r'^verify_user/(?P<auth_key>[\w\+%_& ]+)/$','ccapp.views.verify_user'),
     url(r'^ajax_contact_seller/$','ccapp.views.ajax_contact_seller'),
     url(r'^ajax_delete_thread/$','ccapp.views.ajax_delete_thread'),
     url(r'^ajax_delete_post/$','ccapp.views.ajax_delete_post'),
